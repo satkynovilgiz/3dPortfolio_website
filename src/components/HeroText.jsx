48b8e1db9582b3a93dea8c4bade7318d90820b61
satkynovilgiz <satkynovilgiz008@gmail.com>
2025-08-15 16:39:13 +0600

    first commit

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -18,7 +18,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Ali
+          Hi I'm Ilgiz 
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
