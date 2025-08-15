0117856d3fd511089724617b85347356b0f38656
satkynovilgiz <satkynovilgiz008@gmail.com>
2025-08-15 23:56:24 +0600

    first commit

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -11,6 +11,7 @@ const HeroText = () => {
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       {/* Desktop View */}
       <div className="flex-col hidden md:flex c-space">
+        {/* myname */}
         <motion.h1
           className="text-4xl font-medium"
           variants={variants}
